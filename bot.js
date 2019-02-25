570f989fe1beac61a623054f916635bdac268589
B.N.Hamilton <bh312@kent.ac.uk>
2019-02-25 14:27:11 +0000

    added a new query

--- bot.js
+++ bot.js
@@ -9,6 +9,10 @@ function answerQuery(query) {
   if (v.includes(query, "rain") || v.includes(query, "sun") || v.includes(query, "weather"))
     return "I do not care too much about weather, I'm locked inside a data center.";
   return "Sorry Dave, I cannot do that."
+  
+  if (v.include(query, "tea")
+	  return "I would love some tea, but they have not created one for silicon-based life forms yet."
+  
 }
 
 
