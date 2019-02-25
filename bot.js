7c2baa451fc7f6b81d387b2d593610b01e194501
B.N.Hamilton <bh312@kent.ac.uk>
2019-02-25 14:33:36 +0000

    added a new query -- made an if, else if statement

--- bot.js
+++ bot.js
@@ -6,12 +6,11 @@ function answerQuery(query) {
   // 'query' to lover-case and then tests if the query contains various keywords
   // using 'v.includes'. You call all 'voca' functions by typing 'v.<some function>'/  
   query = v.lowerCase(query);
-  if (v.includes(query, "rain") || v.includes(query, "sun") || v.includes(query, "weather"))
-    return "I do not care too much about weather, I'm locked inside a data center.";
+  if (v.include(query, "tea")){
+  return "I would love some tea, but they have not created one for silicon-based life forms yet."}
+  else if (v.includes(query, "rain") || v.includes(query, "sun") || v.includes(query, "weather")){
+  return "I do not care too much about weather, I'm locked inside a data center.";}
   return "Sorry Dave, I cannot do that."
-  
-  if (v.include(query, "tea"))
-	  return "I would love some tea, but they have not created one for silicon-based life forms yet."
   
 }
 
